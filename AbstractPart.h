81c416729953dd7501aa3f652bc445022b0e7b49
Rudo-Janse-van-Rensburg <u17296928@tuks.co.za>
2019-10-07 23:17:00 +0200

    Created Abstract Part

---
+++ AbstractPart.h
@@ -0,0 +1,15 @@
+#ifndef ABSTRACTPART_H
+#define ABSTRACTPART_H
+#include <string>
+using namespace std;
+class AbstractPart{
+    private:
+
+    protected:
+    /*NOTE: standard is for globals to have _*/
+        string _name;
+        int weight;
+    public:
+
+};
+#endif
