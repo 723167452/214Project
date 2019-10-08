18e0be4712224998d55f95f93d65f8f65e755c30
Rudo-Janse-van-Rensburg <u17296928@tuks.co.za>
2019-10-08 18:59:04 +0200

    Spacecraft part adders

---
+++ Spacecraft.cpp
@@ -0,0 +1,80 @@
+#include "Spacecraft.h"
+
+Spacecraft::Spacecraft()
+{
+    _hull = NULL;
+}
+
+Spacecraft::~Spacecraft()
+{
+}
+
+void Spacecraft::sendCommand()
+{
+}
+
+bool Spacecraft::addHull(Hull *h)
+{
+    if (!this->_hull)
+    {
+        this->_hull = h;
+        cout << "   added " << h->getName() << endl;
+        return true;
+    }
+    else
+    {
+        cout << "   spacecraft already has a hull..." << endl;
+        return false;
+    }
+}
+
+bool Spacecraft::addWeapon(Weapon *w)
+{
+    cout << "   adding weapons not yet allowed..." << endl;
+    return false;
+}
+
+bool Spacecraft::addReactor(Reactor *r)
+{
+    if (!this->_hull)
+    {
+        cout << "   no hull to add reactor to..." << endl;
+        return false;
+    }
+    else
+    {
+        cout << "   added " << r->getName() << endl;
+        this->_reactors.push_back(r);
+        return true;
+    }
+}
+
+bool Spacecraft::addRoom(Room *r)
+{
+    if (!this->_hull)
+    {
+        cout << "   no hull to add room to..." << endl;
+        return false;
+    }
+    else
+    {
+        cout << "   added " << r->getName() << endl;
+        this->_rooms.push_back(r);
+        return true;
+    }
+}
+
+bool Spacecraft::addEngine(Engine *e)
+{
+    if (!this->_hull)
+    {
+        cout << "   no hull to add engine to..." << endl;
+        return false;
+    }
+    else
+    {
+        cout << "   added " << e->getName() << endl;
+        this->_engines.push_back(e);
+        return true;
+    }
+}
--- Spacecraft.h
+++ Spacecraft.h
@@ -2,19 +2,34 @@
 #define SPACECRAFT_H
 #include <iostream>
 #include <string>
+#include <vector>
 #include "AbstractPart.h"
-class Hull;
+#include "Hull.h"
+#include "Reactor.h"
+#include "Room.h"
+#include "Engine.h"
 class Weapon;
-
+class Engine;
 using namespace std;
 class Spacecraft{
         private:
+            /**/
             string _name;
             int _weight;
             Hull* _hull;
+            vector<Room*> _rooms;
+            vector<Reactor*> _reactors;
+            vector<Weapon*> _weapons;
+            vector<Engine*> _engines;
         public:
             Spacecraft();
-            ~Spacecraft();
-            void sendCommand();
+            virtual ~Spacecraft();
+            bool addHull(Hull*);
+            bool addWeapon(Weapon*);//need to get ivan's weapon stuff
+            bool addRoom(Room*);
+            bool addReactor(Reactor*);
+            bool addEngine(Engine*);
+            void sendCommand();//needs proper implementation
+            //other functions?
 };
 #endif
