e1aa5cd04421d56db178d9a4bbff91610b54151f
Rudo-Janse-van-Rensburg <u17296928@tuks.co.za>
2019-10-24 21:17:54 +0200

    making Spacestation branch, and basic class layout

---
+++ Spacestation.h
@@ -0,0 +1,14 @@
+#ifndef SPACESTATION_H
+#define SPACESTATION_H
+#include "Room.h"
+#include "Spacecraft.h"
+using namespace std;
+class Spacestation : public Spacecraft , public Room{
+    private:
+
+    protected:
+
+    public:
+    
+};
+#endif
