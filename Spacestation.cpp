4a908190545c8ccc4439d8314854e6e7fd105f18
Rudo-Janse-van-Rensburg <u17296928@tuks.co.za>
2019-10-24 21:56:28 +0200

    Basic Spacestation

--- Spacestation.cpp
+++ Spacestation.cpp
@@ -1,1 +1,10 @@
 #include "Spacestation.h"
+
+Spacestation* Spacestation::_onlyInstance = 0;
+
+Spacestation* Spacestation::instance(){
+    if(_onlyInstance == 0){
+        _onlyInstance = new Spacestation();
+    }
+    return _onlyInstance;
+}
--- Spacestation.h
+++ Spacestation.h
@@ -2,13 +2,21 @@
 #define SPACESTATION_H
 #include "Room.h"
 #include "Spacecraft.h"
+#include <vector>
 using namespace std;
 class Spacestation : public Spacecraft , public Room{
     private:
-
+        static Spacestation* _onlyInstance;
+        vector<Spacecraft*> _fleet;
     protected:
-
+        Spacestation();
+        Spacestation(const Spacestation&);
+        ~Spacestation();
+        Spacestation& operator=(const Spacestation&);
     public:
-    
+        static Spacestation* instance();
+        void updateStation(Spacestation&);
+
+
 };
 #endif
