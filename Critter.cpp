840cbbe55413f32bb710748c1cdc38564f095327
Dylan-Krajnc <u17029318@tuks.co.za>
2019-10-25 23:14:02 +0200

    critter protoype

---
+++ Critter.cpp
@@ -0,0 +1,74 @@
+/**
+ * @author Dylan Krajnc
+*/
+
+#include "Critter.h"
+
+Critter::Critter() {
+
+}
+
+Critter::~Critter() {
+
+}
+
+void Critter::lowerTolerance() {
+	cout << "The bug did not like that. Tolerance towards you has lowered." << endl;
+	if(toleranceLevel < 5 && toleranceLevel != 0 && currentMood == Friendly) {
+		currentMood = Hostile;
+		cout << "The bug is now hostile, be careful!" << endl;
+	}
+	if(toleranceLevel == 0) {
+		currentMood = Enemy;
+		cout << "The bug is now Enemy. Attack it!" << endl;
+	}
+}
+
+void Critter::increaseTolerance() {
+	cout << "The bug liked that. Tolerance towards you has increased." << endl;
+	if(toleranceLevel > 4 && currentMood != Friendly) {
+		currentMood = Friendly;
+		cout << "The bug is now friendly, that's nice!" << endl;
+	}
+	//can't increase tolerance once the bug is an enemy
+}
+
+SpaceBug::SpaceBug(string n) {
+	name = n;
+	toleranceLevel = 5;
+	currentMood = Hostile;
+	defeated = false;
+}
+
+SpaceBug::~SpaceBug() {
+
+}
+
+void SpaceBug::meleeAttack() {
+	cout << "The spaceBug hurls itself at the ships, making a small dent." << endl;
+}
+
+void SpaceBug::isAttacked() {
+	defeated = true;
+	cout << "The spaceBug has been defeated by the Spacecraft." << endl;
+}
+
+LandCritter::LandCritter(string n) { 
+	name = n;
+	toleranceLevel = 10;
+	currentMood = Friendly;
+	defeated = false;
+}
+
+LandCritter::~LandCritter() {
+
+}
+
+void LandCritter::meleeAttack() {
+	cout << "The landCritter spits at the crew. It is inaffective." << endl;
+}
+
+void LandCritter::isAttacked() {
+	defeated = true;
+	cout << "The landCritter has been defeated by the crew." << endl;
+}
