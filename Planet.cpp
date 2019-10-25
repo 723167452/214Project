a616c3f415aba8f80d1611c8bcefc02c82c7932a
Rudo-Janse-van-Rensburg <u17296928@tuks.co.za>
2019-10-25 20:16:11 +0200

    Proper planet resource, and safety calculatons...

--- Planet.cpp
+++ Planet.cpp
@@ -1,23 +1,37 @@
 #include "Planet.h"
 
-Planet::Planet(string name, bool inhabited, double ores, double oil, double victuals, Safety dangerLevel, PlanetType planetType, vector<Critters *> wildlife)
+Planet::Planet(string name, bool inhabited, double ores, double oil, double victuals,Environment landscape, Safety dangerLevel, PlanetType planetType, vector<Critters *> wildlife)
 {
-    this->_planetName = name;
-    this->_ores = ores;
-    this->_oil = oil;
-    this->_victuals = victuals;
-    this->_danger = dangerLevel;
-    this->_inhabited = (this->_type == PlanetType::solid) ? inhabited : false;
-    this->_type = planetType;
-    this->_wildLife = wildlife;
+    this->_planetName               = name;
+    this->_ores                     = ores;
+    this->_oil                      = oil;
+    this->_victuals                 = victuals;
+    this->_danger                   = dangerLevel;
+    this->_landscape                = landscape;
+    this->_inhabited                = (this->_type == PlanetType::solid) ? inhabited : false;
+    this->_type                     = planetType;
+    this->_terrestrialCritters      = wildlife;
 }
 
 Planet::~Planet()
 {
+
+}
+void Planet::determineSafety(){
+    int safety = _landscape * _nature; 
+    if(safety >= 2){
+        _danger = Safety::safe;
+    }else if(safety = 1){
+        _danger = Safety::moderate;
+    }else{
+        _danger = Safety::hostile;
+    }
+
 }
 
 bool Planet::inhabit()
 {
+    determineSafety();
     if (this->_danger == Safety::safe && !_inhabited && this->_type == PlanetType::solid)
     {
         _inhabited = true;
@@ -26,20 +40,44 @@ bool Planet::inhabit()
     return false;
 }
 
-void Planet::makeSafer()
-{
-    if (this->_type == PlanetType::solid)
-    {
-        switch (this->_danger)
-        {
-        case Safety::safe:
+
+void Planet::tameWildlife(){
+    switch(this->_nature){
+        case Wildlife::untamed :
+            _nature = Wildlife::moderate;
+            break;       
+        case Wildlife::moderate :
+            _nature = Wildlife::tame;
             break;
-        case Safety::moderate:
-            this->_danger = Safety::safe;
+        case Wildlife::tame :
+            _nature = Wildlife::tame;
             break;
-        case Safety::hostile:
-            this->_danger = Safety::moderate;
-            break;
-        }
     }
 }
+
+void Planet::terraform(){
+    switch(this->_landscape){
+        case Environment::Barren :
+            this->_landscape = Environment::Sparse;
+            break;
+        case Environment::Sparse :
+            this->_landscape = Environment::Dense;
+            break;
+        case Environment::Dense :
+            this->_landscape = Environment::Dense;
+            break;
+    }
+}
+
+double Planet::oreOutput(){
+    return _ores * _landscape; 
+}
+
+double Planet::oilOutput(){
+    return _oil * _landscape;
+}
+
+double Planet::victualOutput(){
+    return _victuals * _landscape;
+}
+
--- Planet.h
+++ Planet.h
@@ -6,21 +6,28 @@ using namespace std;
 class Critters;
 class Planet{
     private:
-        enum Safety { hostile, moderate, safe} _danger;
-        enum PlanetType { gas , solid} _type;
+        enum Safety         { hostile = 0   , moderate = 1 ,    safe = 2} _danger;
+        enum PlanetType     { gas     = 0   , solid    = 1              } _type;
+        enum Environment    { Barren  = 0   , Sparse   = 1 ,   Dense = 2} _landscape;
+        enum Wildlife       { untamed = 0   , moderate = 1 ,   tame  = 2} _nature;
         double _ores;
         double _oil;
         double _victuals;
         bool _inhabited;        //planet is inhabited
         string _planetName;
-        vector<Critters*> _wildLife;
+        vector<Critters*> _terrestrialCritters;
     protected:
 
 
     public:
-        Planet(string, bool, double, double, double,Safety, PlanetType , vector<Critters*> );
+        Planet(string, bool, double, double, double,Environment, Safety, PlanetType , vector<Critters*> );
         ~Planet();
+        void determineSafety();
         bool inhabit();
-        void makeSafer();
+        void terraform();
+        void tameWildlife();
+        double oreOutput();
+        double oilOutput();
+        double victualOutput();
 };
 #endif
