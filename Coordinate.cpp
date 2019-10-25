7012fe6a44356abcec70161fed614b978b8cf54c
Rudo-Janse-van-Rensburg <u17296928@tuks.co.za>
2019-10-25 22:21:08 +0200

    Coordinate changed

--- Coordinate.cpp
+++ Coordinate.cpp
@@ -1,52 +1,11 @@
 #include "Coordinate.h"
 
-Coordinate::Coordinate()
+Coordinate::Coordinate(int x, int y)
 {
-    _planet = NULL;
+    _x = x;
+    _y = y;
 }
 
 Coordinate::~Coordinate()
 {
-}
-
-bool Coordinate::addPlanet(Planet *p)
-{
-    if (!_planet)
-    {
-        this->_planet = p;
-        return true;
-    }
-    return false;
-}
-
-bool Coordinate::removeAllySpacecraft(Spacecraft *craft)
-{
-    vector<Spacecraft *>::iterator it = _allyFleet.begin();
-    bool found = false;
-    while ((it != _allyFleet.end()) && !found)
-    {
-        if (*it == craft)
-        {
-            found = true;
-            _allyFleet.erase(it);
-        }
-        ++it;
-    }
-    return found;
-}
-
-bool Coordinate::removeEnemySpacecraft(Spacecraft *craft)
-{
-    vector<Spacecraft *>::iterator it = _enemyFleet.begin();
-    bool found = false;
-    while ((it != _enemyFleet.end()) && !found)
-    {
-        if (*it == craft)
-        {
-            found = true;
-            _enemyFleet.erase(it);
-        }
-        ++it;
-    }
-    return found;
 }
--- Coordinate.h
+++ Coordinate.h
@@ -1,20 +1,11 @@
 #ifndef COORDINATE_H
 #define COORDINATE_H
-#include "Planet.h"
-#include "Spacecraft.h"
 #include <vector>
 class Coordinate{
-    private:
-        Planet* _planet;
-        vector<Spacecraft*> _allyFleet;
-        vector<Spacecraft*> _enemyFleet;
-    protected:
-        
     public:
         Coordinate();
         virtual ~Coordinate();
-        bool addPlanet(Planet*);
-        bool removeAllySpacecraft(Spacecraft*);
-        bool removeEnemySpacecraft(Spacecraft*);
+        int _x;
+        int _y;
 };
 #endif
